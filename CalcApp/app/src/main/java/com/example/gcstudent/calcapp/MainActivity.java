918b4fa6e8f7012d6f66e0a183a4c7bdc9285d57
JustinDL <justin_d_lucas@hotmail.com>
2016-03-11 11:43:56 -0500

    Added text Functioanlity
    title

--- CalcApp/app/src/main/java/com/example/gcstudent/calcapp/MainActivity.java
+++ CalcApp/app/src/main/java/com/example/gcstudent/calcapp/MainActivity.java
@@ -4,15 +4,118 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String text1 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    // TextView edit = (TextView)findViewById(R.id.TextView1);
+    //edit.setText(total);
+    public void ButtonClick(View v){
+        TextView edit = (TextView)findViewById(R.id.textView1);
+        switch (v.getId()){
+
+            case R.id.OneButton:
+                text1 = text1 + "1 ";
+                edit.setText(text1);
+            break;
+            case R.id.TwoButton:
+                text1 = text1 + "2 ";
+                edit.setText(text1);
+                break;
+            case R.id.ThreeButton:
+                text1 = text1 + "3 ";
+                edit.setText(text1);
+                break;
+            case R.id.FourButton:
+                text1 = text1 + "4 ";
+                edit.setText(text1);
+                break;
+            case R.id.FiveButton:
+                text1 = text1 + "5 ";
+                edit.setText(text1);
+                break;
+            case R.id.SixButton:
+                text1 = text1 + "6 ";
+                edit.setText(text1);
+                break;
+            case R.id.SevenButton:
+                text1 = text1 + "7 ";
+                edit.setText(text1);
+                break;
+            case R.id.EightButton:
+                text1 = text1 + "8 ";
+                edit.setText(text1);
+                break;
+            case R.id.NineButton:
+                text1 = text1 + "9 ";
+                edit.setText(text1);
+                break;
+
+            // function buttons
+            //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            case R.id.AddButton:
+                text1 = text1 + "+ ";
+                edit.setText(text1);
+
+
+                break;
+            case R.id.SubButton:
+                text1 = text1 + "- ";
+                edit.setText(text1);
+
+
+                break;
+            case R.id.MultButton:
+                text1 = text1 + "* ";
+                edit.setText(text1);
+
+
+                break;
+            case R.id.DivButton:
+                text1 = text1 + "/ ";
+                edit.setText(text1);
+
+
+                break;
+            case R.id.ClearButton:
+                text1 = "";
+                edit.setText(text1);
+                break;
+            case R.id.EqualButton:
+
+
+
+                break;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
